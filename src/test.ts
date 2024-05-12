68a68ada92c4ec247f504d1393389bbf10584e3f
Tom Heinemeyer <tom.heinemeyer@gmail.com>
2024-05-12 16:28:19 +0200

    Split tests

--- src/test.ts
+++ src/test.ts
@@ -19,13 +19,22 @@ describe("isObject", () => {
   it("undefined is not an object", () => {
     expect(isObject(undefined)).toBe(false)
   })
-  it("valid cases", () => {
+  it("an empty object is an object", () => {
     expect(isObject({})).toBe(true)
+  })
+  it("an object created from Object.create with an empty object is an object", () => {
     expect(isObject(Object.create({}))).toBe(true)
+  })
+  it("an object created from Object.create with the object prototype is an object", () => {
     expect(isObject(Object.create(Object.prototype))).toBe(true)
+  })
+  it("an object created from Object.create with null is an object", () => {
     expect(isObject(Object.create(null))).toBe(true)
-    expect(isObject({})).toBe(true)
+  })
+  it("an object instatiated with the new keyword is an object", () => {
     expect(isObject(new Error())).toBe(true)
+  })
+  it("a regular expression is an object", () => {
     expect(isObject(/foo/)).toBe(true)
   })
 })
