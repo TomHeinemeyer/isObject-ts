7b71d9ec8989c2c7cbfc0e295deaf5b102dbfa94
Tom Heinemeyer <tom.heinemeyer@gmail.com>
2024-05-19 13:31:41 +0200

    Add JSDoc

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,18 @@
  * Released under the MIT License.
  */
 
+/**
+ * @param {unknown} value The value to be checked.
+ * @returns {boolean} True, if the value is an object that can be keyed into, false otherwise.
+ *
+ * @example
+ * // logs true
+ * console.debug(isObject({key: value}))
+ *
+ * @example
+ * // logs false
+ * console.debug(isObject(null))
+ */
 export default function isObject(
   value: unknown
 ): value is Record<string | number | symbol, unknown> {
